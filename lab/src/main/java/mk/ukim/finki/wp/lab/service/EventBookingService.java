8107f4ca2ff6e899b5ea7ea174a5d470383f6e0f
MarkoKajtazi <marko.kajtazi@students.finki.ukim.mk>
2024-12-20 16:25:06 +0100

    Delete lab directory

--- lab/src/main/java/mk/ukim/finki/wp/lab/service/EventBookingService.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.wp.lab.service;
-
-import mk.ukim.finki.wp.lab.model.EventBooking;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EventBookingService {
-    EventBooking placeBooking(String eventName, String attendeeName, String attendeeAddress, int numberOfTickets);
-}
